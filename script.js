c52505025512d576a37db9349b1ec109a852c15c
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-07-18 10:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-
-	const byteSize = (str) => {
-  let totalBytes = 0;
-  for (let i = 0; i < str.length; i++) {
-    const charCode = str.charCodeAt(i);
-    if (charCode <= 0x7f) {
-      totalBytes += 1; // ASCII character
-    } else if (charCode <= 0x7ff) {
-      totalBytes += 2; // 2-byte character
-    } else if (charCode <= 0xffff) {
-      totalBytes += 3; // 3-byte character
-    } else {
-      totalBytes += 4; // 4-byte character
-    }
-  }
-  return totalBytes;
-
-
+const byteSize = (str) => {
+  const encoder = new TextEncoder();
+  const encodedString = encoder.encode(str);
+  return encodedString.length;
 };
-
-// Do not change th
-const str = prmpt("Enter some string.");
+const str = prompt("Enter some string.");
 alert(byteSize(str));
